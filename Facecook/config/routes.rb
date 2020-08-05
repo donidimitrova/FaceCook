534a440989d6db086b38bdf5e6a3f54a4f0c171d
donidimitrova <dimitrovadonika@gmail.com>
2020-08-05 18:05:06 +0200

    bozza pagine abot e contact

--- Facecook/config/routes.rb
+++ Facecook/config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   #Update Routes
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'pages#home'
+  root 'pages#home'
+  get 'help' => 'pages#help' 
+  get 'about' => 'pages#about' 
+  get 'contact' => 'pages#contact'
 end
