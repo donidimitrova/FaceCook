5f9844f239c8a98f79ec176b9994062e2486c624
donidimitrova <dimitrovadonika@gmail.com>
2020-08-08 13:03:43 +0200

    registration insert field in database

--- Facecook/config/routes.rb
+++ Facecook/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   #Update Routes
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  
   root to: 'pages#home'
   root 'pages#home'
   get 'help' => 'pages#help' 
