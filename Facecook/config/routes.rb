013eecc0340aa922ce4ca7dd5a37300f7d4c585e
donidimitrova <dimitrovadonika@gmail.com>
2020-08-09 19:03:30 +0200

    ok responsive registration page

--- Facecook/config/routes.rb
+++ Facecook/config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   #Update Routes
 
-  devise_for :users , controllers: {registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks'}, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register"}  
+  devise_for :users , controllers: { :omniauth_callbacks =>'users/omniauth_callbacks',
+                                      :registrations => 'registrations'}
   resources :users, only: [:new, :create, :edit, :update]
   resources :pages
   resources :profile
 
   root to: 'pages#home'
   root 'pages#home'
+
+
+
   get 'help' => 'pages#help' 
   get 'about' => 'pages#about' 
   get 'contact' => 'pages#contact'
