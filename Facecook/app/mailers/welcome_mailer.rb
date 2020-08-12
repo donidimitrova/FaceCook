01bb6ca3057bf9abb5a0389f4053a5332141c6d6
donidimitrova <dimitrovadonika@gmail.com>
2020-08-12 17:12:48 +0200

    error 'subject' in welcome_mailers.rb

--- Facecook/app/mailers/welcome_mailer.rb
+++ Facecook/app/mailers/welcome_mailer.rb
@@ -1,6 +1,6 @@
 class WelcomeMailer < ApplicationMailer
     def welcome_send(user)
         @user = user
-        mail to: user.email, subjuct: "Registrazione avvenuta con successo", from: "dimitrovadonika@gmail.com"
+        mail to: user.email, subject: "Registrazione avvenuta con successo", from: "dimitrovadonika@gmail.com"
     end
 end
