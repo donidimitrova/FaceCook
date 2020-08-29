d981a49a0db5045117735828054c80dff5b26814
Ingegneria Informatica e Automatica <manuelgrossi03@gmail.com>
2020-08-29 15:35:30 +0200

    aggiunto modello category

--- Facecook/db/schema.rb
+++ Facecook/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_29_131517) do
+ActiveRecord::Schema.define(version: 2020_08_29_133304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "nome"
